69da24979839b02d3b6fd5e52a7e55860db1cf81
renlu <believe_rl@163.com>
2022-03-30 14:55:15 +0800

    升级包管理

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import router from './router'
 import store from './store'
 
 import ElementPlus from 'element-plus'
-import 'element-plus/lib/theme-chalk/index.css'
+import 'element-plus/dist/index.css'
 
 import '@/assets/theme/global.scss'
 
